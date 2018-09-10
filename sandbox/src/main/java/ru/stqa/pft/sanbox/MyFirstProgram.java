27ea26cad57f3d00af26ba0177715670920dcf07
webFreePpl <41953284+webFreeppl@users.noreply.github.com>
2018-09-10 22:09:45 +0300

    +1

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -7,11 +7,15 @@ public class MyFirstProgram {
     hello("user");
     hello("Andrey");
 
+    Square s = new Square(5);
+    s.l = 5;
+
     double l = 5;
-    System.out.println("Площадь квадрата со стороной "+l+"="+area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со стороной "+a+"и"+b+"="+area(a,b));
+    System.out.println("Площадь квадрата со стороной "+s.l+" = "+area(s));
+
+    Rectangle r = new Rectangle(4,6);
+
+    System.out.println("Площадь прямоугольника со стороной "+r.a+" и "+r.b+" = "+area(r));
   }
 
 
@@ -20,11 +24,11 @@ public class MyFirstProgram {
     System.out.println("Hello, "+somebody+"!");
   }
 
-  public static double area (double len){
-    return len*len;
+  public static double area (Square s){
+    return s.l*s.l;
   }
 
-  public static double area(double a, double b) {
-    return a*b;
+  public static double area(Rectangle r) {
+    return r.a*r.b;
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sanbox;
+
+public class Square {
+  public double l;
+
+  public Square(double len){
+    this.l = len;
+  }
+}
